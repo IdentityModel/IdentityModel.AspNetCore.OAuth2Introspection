8b34d4ebb6a53e982b46f94ed23316c1869d2837
Dominick Baier <dbaier@leastprivilege.com>
2015-12-26 21:53:01 +0100

    1st commit

---
+++ src/IdentityModel.AspNet.OAuth2Introspection/Infrastructure/StringExtensions.cs
@@ -0,0 +1,30 @@
+﻿using System.Diagnostics;
+
+namespace IdentityModel.AspNet.OAuth2Introspection
+{
+    internal static class StringExtensions
+    {
+        [DebuggerStepThrough]
+        public static string EnsureTrailingSlash(this string input)
+        {
+            if (!input.EndsWith("/"))
+            {
+                return input + "/";
+            }
+
+            return input;
+        }
+
+        [DebuggerStepThrough]
+        public static bool IsMissing(this string value)
+        {
+            return string.IsNullOrWhiteSpace(value);
+        }
+
+        [DebuggerStepThrough]
+        public static bool IsPresent(this string value)
+        {
+            return !string.IsNullOrWhiteSpace(value);
+        }
+    }
+}
---
+++ src/IdentityModel.AspNet.OAuth2Introspection/Infrastructure/TokenRetrieval.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNet.Http;
+using System;
+using System.Linq;
+
+namespace IdentityModel.AspNet.OAuth2Introspection
+{
+    public static class TokenRetrieval
+    {
+        public static Func<HttpRequest, string> FromAuthorizationHeader(string scheme = "Bearer")
+        {
+            return (request) =>
+            {
+                string authorization = request.Headers["Authorization"];
+
+                if (string.IsNullOrEmpty(authorization))
+                {
+                    return null;
+                }
+
+                if (authorization.StartsWith(scheme + " ", StringComparison.OrdinalIgnoreCase))
+                {
+                    return authorization.Substring(scheme.Length + 1).Trim();
+                }
+
+                return null;
+            };
+        }
+
+        public static Func<HttpRequest, string> FromQueryString(string name = "access_token")
+        {
+            return (request) =>
+            {
+                return request.Query[name].FirstOrDefault();
+            };
+        }
+    }
+}
