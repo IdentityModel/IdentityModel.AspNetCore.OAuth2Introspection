a7955c68a28a6a8544f1fa9c82a10aff2ac5141e
Dominick Baier <dbaier@leastprivilege.com>
2019-03-08 10:31:31 +0100

    remove internal IntrospectionClient

--- src/Infrastructure/IntrospectionClientOptions.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace IdentityModel.AspNetCore.OAuth2Introspection.Infrastructure
-{
-    internal class IntrospectionClientOptions
-    {
-        public string Address { get; internal set; }
-        public string ClientId { get; internal set; }
-        public string ClientSecret { get; internal set; }
-    }
-}
