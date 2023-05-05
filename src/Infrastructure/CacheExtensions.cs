1e41dfb438757e61bb4892a4d87f790ac8f084e9
Christoph Bühler <buehler@users.noreply.github.com>
2023-05-05 09:51:48 +0200

    Changes according to review

--- src/Infrastructure/CacheExtensions.cs
+++ src/Infrastructure/CacheExtensions.cs
@@ -46,12 +46,6 @@ namespace IdentityModel.AspNetCore.OAuth2Introspection
         public static async Task SetClaimsAsync(this IDistributedCache cache, OAuth2IntrospectionOptions options, string token, IEnumerable<Claim> claims, TimeSpan duration, ILogger logger)
         {
             var expClaim = claims.FirstOrDefault(c => c.Type == JwtClaimTypes.Expiration);
-            if (expClaim == null && !options.CacheIgnoreMissingExp)
-            {
-                Log.NoExpClaimFound(logger, null);
-                return;
-            }
-
             var now = DateTimeOffset.UtcNow;
             var expiration = expClaim == null
                 ? now + duration
