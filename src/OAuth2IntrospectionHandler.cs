c526a3a4224338e3174b1ce34eb8cb8390e34ad4
Dominick Baier <dbaier@leastprivilege.com>
2024-09-09 08:04:18 +0200

    Update file headers

--- src/OAuth2IntrospectionHandler.cs
+++ src/OAuth2IntrospectionHandler.cs
@@ -1,4 +1,4 @@
-// Copyright (c) Dominick Baier & Brock Allen. All rights reserved.
+// Copyright (c) Duende Software. All rights reserved.
 // Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
 
 using System;
